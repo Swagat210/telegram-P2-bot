2cbe9123e4b7a8918357e76655d2d428bef0202b
Swagat210 <swagatkumar210@gmail.com>
2025-11-06 00:26:59 +0530

    Modify app.py to run with uvicorn
    Updated app.py to use uvicorn for running the application.
    
    Signed-off-by: Swagat210 <swagatkumar210@gmail.com>

--- app.py
+++ app.py
@@ -1,1 +1,5 @@
-python app.py
+from uvicorn import run
+from src.main import app   # adjust import to your app object
+
+if __name__ == "__main__":
+    run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
