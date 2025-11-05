edb30996d8be76001788e3ab8df16dfeff27c05b
Swagat210 <swagatkumar210@gmail.com>
2025-11-05 23:27:50 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -4,9 +4,9 @@ import os
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv('BOT_TOKEN')
-MERCHANT_UPI = os.getenv('MERCHANT_UPI')
-CHANNEL_ID = os.getenv('CHANNEL_ID')
+BOT_TOKEN = os.getenv('8411398051:AAFMb3MmkumrGKQ3uy7V1tEltvUOtENmbDI')
+MERCHANT_UPI = os.getenv('8260758004@ptsbi')
+CHANNEL_ID = os.getenv('3059982885')
 WEBHOOK_SECRET = os.getenv('WEBHOOK_SECRET')
 HOST = os.getenv('HOST', 'http://localhost:5611')
 DB_PATH = Path(__file__).parent.parent / 'data' / 'bot.db'
